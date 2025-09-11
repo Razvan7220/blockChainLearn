0e79b7b41898e88b964d4c3b279965e2cc8f58a7
Razvan7220 <borcanpetrurazvan2000@gmail.com>
2025-09-11 15:54:35 +0300

    FundMe.sol

---
+++ contracts/FundMe.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract FundMe {
+    mapping(address => uint256) public addressToAmountFunded;
+    address public owner;
+    address[] public funders;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function fund() public payable {
+        uint256 minimumUSD = 3 * 10 ** 18;
+        require(
+            getConversionRate(msg.value) >= minimumUSD,
+            "You need to spent more ether"
+        );
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
+    }
+
+    function getPrice() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        return uint256(price * 1e10);
+    }
+
+    function getConversionRate(
+        uint256 ethAmount
+    ) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner");
+        _;
+    }
+    function withdraw() public payable onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            addressToAmountFunded[funders[funderIndex]] = 0;
+        }
+        funders = new address[](0);
+    }
+}
